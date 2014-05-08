60897420c1ee3b2a4456aafedeb65299e42052d9
nelio <nelio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-08 15:32:51 +0000

    Uploading version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/external-featured-image/tags/1.0.3@910565 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin/edit-post.php
@@ -0,0 +1,102 @@
+<?php
+
+// Creating box
+add_action( 'add_meta_boxes', 'nelioefi_add_url_metabox' );
+function nelioefi_add_url_metabox() {
+
+	add_meta_box(
+		'nelioefi_url_metabox',
+		'External Featured Image',
+		'nelioefi_url_metabox',
+		'post',
+		'side',
+		'default'
+	);
+
+	add_meta_box(
+		'nelioefi_url_metabox',
+		'External Featured Image',
+		'nelioefi_url_metabox',
+		'page',
+		'side',
+		'default'
+	);
+
+}
+
+function nelioefi_url_metabox( $post ) {
+	$nelioefi_url = get_post_meta( $post->ID, '_nelioefi_url', true );
+	$has_img = strlen( $nelioefi_url ) > 0;
+
+	if ( $has_img ) { ?>
+	<div id="nelioefi_preview_block"><?php
+	} else { ?>
+	<div id="nelioefi_preview_block" style="display:none;"><?php
+	} ?>
+		<div id="nelioefi_image_wrapper" style="<?php
+			echo (
+				'width:100%;' .
+				'max-width:300px;' .
+				'height:200px;' .
+				'margin-top:10px;' .
+				'background:url(' . $nelioefi_url . ') no-repeat center center; ' .
+				'-webkit-background-size:cover;' .
+				'-moz-background-size:cover;' .
+				'-o-background-size:cover;' .
+				'background-size:cover;' );
+			?>">
+		</div>
+
+	<?php
+	if ( strlen( $nelioefi_url ) > 0 ) { ?>
+		<a id="nelioefi_remove_button" href="#" onClick="javascript:nelioefiRemoveFeaturedImage();">Remove featured image</a>
+		<script>
+		function nelioefiRemoveFeaturedImage() {
+			jQuery("#nelioefi_preview_block").hide();
+			jQuery("#nelioefi_image_wrapper").hide();
+			jQuery("#nelioefi_remove_button").hide();
+			jQuery("#nelioefi_url").val('');
+			jQuery("#nelioefi_controls").show();
+		}
+		</script>
+		<?php
+	} ?>
+	</div><?php
+
+	if ( $has_img ) { ?>
+	<div id="nelioefi_controls" style="display:none;"><?php
+	} else { ?>
+	<div id="nelioefi_controls"><?php
+	} ?>
+		<input type="text" placeholder="Image URL" style="width:100%;margin-top:10px;"
+			id="nelioefi_url" name="nelioefi_url"
+			value="<?php echo esc_attr( $nelioefi_url ); ?>" />
+		<div style="text-align:right;margin-top:10px;">
+			<a class="button" id="ext_feat_img-preview" onClick="javascript:nelioefiPreview();">Preview</a>
+			<script>
+			function nelioefiPreview() {
+				jQuery("#nelioefi_preview_block").show();
+				jQuery("#nelioefi_image_wrapper").css('background-image', "url('" + jQuery("#nelioefi_url").val() + "')" );
+				jQuery("#nelioefi_image_wrapper").show();
+			}
+			</script>
+		</div>
+	</div>
+	<?php
+}
+
+add_action( 'save_post', 'nelioefi_save_url' );
+function nelioefi_save_url( $post_ID ) {
+	global $post;
+	if ( isset( $_POST['nelioefi_url'] ) ) {
+		update_post_meta( $post_ID, '_nelioefi_url', strip_tags( $_POST['nelioefi_url'] ) );
+		$wordpress_featured_image = get_post_meta( $post_ID, '_thumbnail_id', true );
+		if ( !$wordpress_featured_image || $wordpress_featured_image == -1 ) {
+			if ( strlen( strip_tags( $_POST['nelioefi_url'] ) ) > 0 )
+				update_post_meta( $post_ID, '_thumbnail_id', -1 );
+			else
+				delete_post_meta( $post_ID, '_thumbnail_id' );
+		}
+	}
+}
+
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Nelio External Featured Image ===
+Contributors: nelio
+Tags: external, url, featured image
+Requires at least: 3.3
+Tested up to: 3.9
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Use external images from anywhere as the featured image of your pages and
+posts.
+
+== Description ==
+
+Are you using an external service for storing your images? Then you'd probably
+like to use those images as featured images for your pages and posts. This
+plugin lets you do this easily!
+
+
+**Notice**
+
+Every time an image is uploaded in the media library, WordPress automatically
+creates alternative sizes of that image (thumbnail, medium, large and full).
+Themes may then choose among these different versions when displaying the an
+image in a post. Thumbnails do also exist for featured images. Moreover, themes
+may registerd their own alternative image sizes. For example, WordPress'
+default theme TwentyFourteen defines a thumbnail size called
+"twentyfourteen-full-width" whose dimensions are 1038x576.
+
+This plugin uses the alternative-size information (which your theme uses for
+rendering a featured image) for scaling and cropping external featured images
+via CSS on your users' browsers.
+
+_Featured image by
+[Cubmundo](https://www.flickr.com/photos/cubmundo/6748759375)_
+
+
+== Changelog ==
+
+= 1.0.3 =
+* Improved image sizing. Now, the plugin uses the sizes the theme defines and
+tries to scale and crop the external image for its proper display.
+
+
+= 1.0.0 =
+* First release.
+
+
+== Upgrade Notice ==
+
+= 1.0.3 =
+External images are now scaled to theme's thumbnail sizes.
+
